36601b974addaab9ed32bb79e826097fb1b863d0
clovisphere <clovis.mugaruka@gmail.com>
2023-08-13 10:57:46 +0300

    Cleanup 🛁: make all pattern matching cases align

--- lists/pset1.ml
+++ lists/pset1.ml
@@ -2,6 +2,6 @@
 
 let rec last (xs: 'a list): 'a option =
     match xs with
-    | []  -> None
-    | [x] -> Some x
+    | []        -> None
+    | [x]       -> Some x
     | _ :: rest -> last rest
