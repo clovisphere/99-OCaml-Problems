a0cb9190ea022d5b76a850afd9fe2dcdafa2f176
clovisphere <clovis.mugaruka@gmail.com>
2023-08-14 11:44:06 +0300

    Update: rename function 'rev' to 'reverse'

--- lists/pset5.ml
+++ lists/pset5.ml
@@ -1,12 +1,12 @@
 (* reverse a list *)
 
-let rec rev (xs: 'a list): ('a list) =
+let rec reverse (xs: 'a list): ('a list) =
   match xs with
   | []          -> []
   | [x]         -> [x]
-  | x :: rest   -> rev rest @ [x]
+  | x :: rest   -> reverse rest @ [x]
 
-let rev_tail_recursive (xs: 'a list): ('a list) =
+let reverse_tail_recursive (xs: 'a list): ('a list) =
     let rec fn acc = function
         | []        -> acc
         | x :: rest -> fn (x :: acc) rest
