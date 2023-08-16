48c65ae9228dcb77bb517a02427c75f5705058b3
clovisphere <clovis.mugaruka@gmail.com>
2023-08-16 19:06:31 +0300

    Add solution to the 'factorial' problem using tail recursion

---
+++ algorithm/factorial.ml
@@ -0,0 +1,8 @@
+(* factorial of n implementation using tail recursion *)
+
+let factorial (n: int): int =
+    let rec go (acc: int) = function
+        | 1 -> acc
+        | n -> go (acc + n) (n - 1)
+    in
+    go 1 n;;
